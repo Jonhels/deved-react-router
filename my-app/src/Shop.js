b2195588ac0d10c03796005366cc61d82b4ad1d4
Jonhels <jonhels8888@gmail.com>
2020-12-13 20:02:47 +0100

    stlying, fixed fetch

--- my-app/src/Shop.js
+++ my-app/src/Shop.js
@@ -1,29 +1,34 @@
-import react, {useState, useEffect} from 'react';
+import react, { useState, useEffect } from 'react';
 import './App.css';
 
 function Shop() {
 
   useEffect(() => {
-      fetchItems();
-  },[]);
+    fetchItems();
+  }, []);
 
   const [items, setItems] = useState([]);
 
   const fetchItems = async () => {
     const data = await fetch(
-        'https://fortnite-api.com/v2/shop/br'
+      'https://fortnite-api.com/v2/shop/br'
     );
-    
+
     const items = await data.json();
     console.log(items.data.daily.entries);
     setItems(items.data.daily.entries);
   };
 
   return (
-    <div>
-        {items.map(item => (
-          <h1> {items.devName}</h1>
-        ))}
+    <div className="shop">
+      {items.map(item => (
+        item.items.map(el => (
+          <div className="shop-item">
+          <img src={el.images.smallIcon}/>
+          <h1>{el.name}</h1>
+          </div>
+          ))
+      ))}
     </div>
   );
 }
